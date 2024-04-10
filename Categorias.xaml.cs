0e77c18a0df8ff1dc3ebd154b323a95495074b0e
Nicol-Melgarejo <146491477+Nicol-Melgarejo@users.noreply.github.com>
2024-04-10 18:42:30 -0500

    Add files via upload

---
+++ Categorias.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab04
+{
+    /// <summary>
+    /// Lógica de interacción para Categorias.xaml
+    /// </summary>
+    public partial class Categorias : Window
+    {
+        public Categorias()
+        {
+            InitializeComponent();
+
+            string connectionString = "Data Source=NICOL\\MSSQLSERVER01; Initial Catalog=NeptunoBD; User Id=User01; " +
+                "Password=123456";
+            List<Categoria> categorias = new List<Categoria>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("USP_ListarCategorias", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int idcategoria = reader.GetInt32(reader.GetOrdinal("idcategoria"));
+                                string nombrecategoria = reader.GetString(reader.GetOrdinal("nombrecategoria"));
+                                string descripcion = reader.GetString(reader.GetOrdinal("descripcion"));
+
+                                categorias.Add(new Categoria { idcategoria = idcategoria, nombrecategoria = nombrecategoria, descripcion = descripcion });
+                            }
+                        }
+                    }
+                }
+
+                dataGridCategorias.ItemsSource = categorias;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        public class Categoria
+        {
+            public int idcategoria { get; set; }
+            public string nombrecategoria { get; set; }
+            public string descripcion { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window ventanaActual = Window.GetWindow(this);
+            ventanaActual.Close();
+        }
+    }
+}
---
+++ Productos.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+
+namespace Lab04
+{
+    /// <summary>
+    /// Lógica de interacción para Productos.xaml
+    /// </summary>
+    public partial class Productos : Window
+    {
+        public Productos()
+        {
+            InitializeComponent();
+
+            string connectionString = "Data Source=NICOL\\MSSQLSERVER01; Initial Catalog=NeptunoBD; User Id=User01; " +
+               "Password=123456";
+
+            List<Producto> productos = new List<Producto>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("USP_ListarProductos", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int idproducto = reader.GetInt32(reader.GetOrdinal("idproducto"));
+                                string nombreProducto = reader.GetString(reader.GetOrdinal("nombreProducto"));
+                                int idProveedor = reader.GetInt32(reader.GetOrdinal("idProveedor"));
+                                int idCategoria = reader.GetInt32(reader.GetOrdinal("idCategoria"));
+
+                                productos.Add(new Producto { idproducto = idproducto, nombreProducto = nombreProducto, idProveedor = idProveedor, idCategoria = idCategoria });
+                            }
+                        }
+                    }
+                }
+
+                dataGridProductos.ItemsSource = productos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        public class Producto
+        {
+            public int idproducto { get; set; }
+            public string nombreProducto { get; set; }
+            public int idProveedor { get; set; }
+            public int idCategoria { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window ventanaActual = Window.GetWindow(this);
+            ventanaActual.Close();
+        }
+    }
+}
